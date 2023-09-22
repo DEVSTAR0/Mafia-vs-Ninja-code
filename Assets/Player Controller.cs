edaaccf61d50114de47f4a3ed407e3a75c147b9d
DEVSTAR0 <145032325+DEVSTAR0@users.noreply.github.com>
2023-09-22 12:42:09 +0100

    Work

--- Assets/Player Controller.cs
+++ Assets/Player Controller.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject groundChecker;
+    public LayerMask whatIsGround;
+
+    public float maxSpeed = 5.0f;
+    bool isOnGround = false;
+
     //Create a referance to the Rigidbody2D so we can manipulate it
-    Rigidbody2D playerObject;
+    Rigidbody2D  playerObject;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +27,10 @@ public class PlayerController : MonoBehaviour
         float movementValueX = Input.GetAxis("Horizontal");
 
         //Change the x velocity of the Rigidbody2D to be equal to the movement value
-        playerObject.velocity = new Vector2(movementValueX, playerObject.velocity.y);
-    }
+        playerObject.velocity = new Vector2 (movementValueX*10, playerObject.velocity.y);
 
+        //Check to see if the ground check object is touching the ground
+        isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
+    }
+        
 }
