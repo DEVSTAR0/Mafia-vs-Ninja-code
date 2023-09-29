d4b2ab5aeb1c3ef208d81584d4e737e58b17b910
DEVSTAR0 <145032325+DEVSTAR0@users.noreply.github.com>
2023-09-29 12:41:31 +0100

    work

--- Assets/PlayerConrol.cs
+++ Assets/PlayerConrol.cs
@@ -34,3 +34,23 @@ public class PlayerController : MonoBehaviour
     }
         
 }
+
+public class PhysicsJump : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 10;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
