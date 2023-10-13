463f8a9e69f34b8a3cc3070129d08bbe8ab0f124
DEVSTAR0 <145032325+DEVSTAR0@users.noreply.github.com>
2023-10-13 10:44:11 +0100

    Update

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour{
-
+public class Main : MonoBehaviour
+{
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
 }
+
--- Assets/PlayerConrol.cs
+++ Assets/PlayerConrol.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerConrol : MonoBehaviour
 {
     public GameObject groundChecker;
     public LayerMask whatIsGround;
@@ -12,13 +12,13 @@ public class PlayerController : MonoBehaviour
     bool isOnGround = false;
 
     //Create a referance to the Rigidbody2D so we can manipulate it
-    Rigidbody2D  playerObject;
+    Rigidbody2D playerObject;
 
     // Start is called before the first frame update
     void Start()
     {
-     //Find the Rigidbody2D component that is attatched to the same object as this script
-     playerObject = GetComponent<Rigidbody2D>();
+        //Find the Rigidbody2D component that is attatched to the same object as this script
+        playerObject = GetComponent<Rigidbody2D>();
     }
 
     private float GetJumpForce()
@@ -33,18 +33,31 @@ public class PlayerController : MonoBehaviour
         float movementValueX = Input.GetAxis("Horizontal");
 
         //Change the x velocity of the Rigidbody2D to be equal to the movement value
-        playerObject.velocity = new Vector2 (movementValueX * 10, playerObject.velocity.y);
+        playerObject.velocity = new Vector2(movementValueX * 10, playerObject.velocity.y);
 
         //Check to see if the ground check object is touching the ground
         isOnGround = Physics2D.OverlapCircle(groundChecker.transform.position, 1.0f, whatIsGround);
-       
+
         if ((isOnGround == true) && (Input.GetAxis("Jump") > 0.0f))
         {
-            playerObject.AddForce(Vector2.up*jumpForce);
+            playerObject.AddForce(Vector2.up * jumpForce);
 
         }
-    }
+        Animator anim;
 
-        
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            anim.SetFloat("Speed", Mathf.Abs(movementValueX));
+            anim.SetBool("IsOnGround", isOnGround);
+        }
+    }
 }
+
+
+ 
 
